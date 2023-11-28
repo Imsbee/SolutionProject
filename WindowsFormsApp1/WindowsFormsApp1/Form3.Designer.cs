d35d66ca2613bd53f5dca0deba5be57495773229
dnut0121 <q7750@naver.com>
2023-11-28 13:11:28 +0900

    3스테이지 이미지 추가

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form3.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.Wall3 = new System.Windows.Forms.PictureBox();
+            this.UpKey1 = new System.Windows.Forms.PictureBox();
+            this.Wall5 = new System.Windows.Forms.PictureBox();
+            this.One1 = new System.Windows.Forms.PictureBox();
+            this.Hurdle = new System.Windows.Forms.PictureBox();
+            this.SideKey = new System.Windows.Forms.PictureBox();
+            this.Wall4 = new System.Windows.Forms.PictureBox();
+            this.Star1 = new System.Windows.Forms.PictureBox();
+            this.Wall2 = new System.Windows.Forms.PictureBox();
+            this.Wall1 = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.UpKey2 = new System.Windows.Forms.PictureBox();
+            this.UpKey3 = new System.Windows.Forms.PictureBox();
+            this.Star5 = new System.Windows.Forms.PictureBox();
+            this.Star4 = new System.Windows.Forms.PictureBox();
+            this.Star3 = new System.Windows.Forms.PictureBox();
+            this.Star2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpKey1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hurdle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SideKey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpKey2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpKey3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Wall3
+            // 
+            this.Wall3.BackColor = System.Drawing.Color.Gray;
+            this.Wall3.Location = new System.Drawing.Point(875, 447);
+            this.Wall3.Name = "Wall3";
+            this.Wall3.Size = new System.Drawing.Size(220, 40);
+            this.Wall3.TabIndex = 15;
+            this.Wall3.TabStop = false;
+            // 
+            // UpKey1
+            // 
+            this.UpKey1.BackColor = System.Drawing.Color.Transparent;
+            this.UpKey1.Image = ((System.Drawing.Image)(resources.GetObject("UpKey1.Image")));
+            this.UpKey1.Location = new System.Drawing.Point(1101, 447);
+            this.UpKey1.Name = "UpKey1";
+            this.UpKey1.Size = new System.Drawing.Size(40, 40);
+            this.UpKey1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UpKey1.TabIndex = 33;
+            this.UpKey1.TabStop = false;
+            // 
+            // Wall5
+            // 
+            this.Wall5.BackColor = System.Drawing.Color.Gray;
+            this.Wall5.Location = new System.Drawing.Point(961, 119);
+            this.Wall5.Name = "Wall5";
+            this.Wall5.Size = new System.Drawing.Size(125, 40);
+            this.Wall5.TabIndex = 40;
+            this.Wall5.TabStop = false;
+            // 
+            // One1
+            // 
+            this.One1.BackColor = System.Drawing.Color.White;
+            this.One1.Image = ((System.Drawing.Image)(resources.GetObject("One1.Image")));
+            this.One1.Location = new System.Drawing.Point(875, 113);
+            this.One1.Name = "One1";
+            this.One1.Size = new System.Drawing.Size(40, 46);
+            this.One1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.One1.TabIndex = 41;
+            this.One1.TabStop = false;
+            // 
+            // Hurdle
+            // 
+            this.Hurdle.BackColor = System.Drawing.Color.Transparent;
+            this.Hurdle.Image = ((System.Drawing.Image)(resources.GetObject("Hurdle.Image")));
+            this.Hurdle.Location = new System.Drawing.Point(920, 165);
+            this.Hurdle.Name = "Hurdle";
+            this.Hurdle.Size = new System.Drawing.Size(40, 40);
+            this.Hurdle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Hurdle.TabIndex = 43;
+            this.Hurdle.TabStop = false;
+            // 
+            // SideKey
+            // 
+            this.SideKey.BackColor = System.Drawing.Color.Transparent;
+            this.SideKey.Image = ((System.Drawing.Image)(resources.GetObject("SideKey.Image")));
+            this.SideKey.Location = new System.Drawing.Point(875, 165);
+            this.SideKey.Name = "SideKey";
+            this.SideKey.Size = new System.Drawing.Size(40, 40);
+            this.SideKey.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.SideKey.TabIndex = 45;
+            this.SideKey.TabStop = false;
+            // 
+            // Wall4
+            // 
+            this.Wall4.BackColor = System.Drawing.Color.Gray;
+            this.Wall4.Location = new System.Drawing.Point(1143, 42);
+            this.Wall4.Name = "Wall4";
+            this.Wall4.Size = new System.Drawing.Size(40, 445);
+            this.Wall4.TabIndex = 46;
+            this.Wall4.TabStop = false;
+            // 
+            // Star1
+            // 
+            this.Star1.BackColor = System.Drawing.Color.Transparent;
+            this.Star1.Image = ((System.Drawing.Image)(resources.GetObject("Star1.Image")));
+            this.Star1.Location = new System.Drawing.Point(349, 401);
+            this.Star1.Name = "Star1";
+            this.Star1.Size = new System.Drawing.Size(40, 40);
+            this.Star1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star1.TabIndex = 47;
+            this.Star1.TabStop = false;
+            // 
+            // Wall2
+            // 
+            this.Wall2.BackColor = System.Drawing.Color.Gray;
+            this.Wall2.Location = new System.Drawing.Point(349, 447);
+            this.Wall2.Name = "Wall2";
+            this.Wall2.Size = new System.Drawing.Size(40, 40);
+            this.Wall2.TabIndex = 48;
+            this.Wall2.TabStop = false;
+            // 
+            // Wall1
+            // 
+            this.Wall1.BackColor = System.Drawing.Color.Gray;
+            this.Wall1.Location = new System.Drawing.Point(0, 42);
+            this.Wall1.Name = "Wall1";
+            this.Wall1.Size = new System.Drawing.Size(40, 445);
+            this.Wall1.TabIndex = 49;
+            this.Wall1.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Transparent;
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(920, 401);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(40, 40);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Ball.TabIndex = 34;
+            this.Ball.TabStop = false;
+            // 
+            // UpKey2
+            // 
+            this.UpKey2.BackColor = System.Drawing.Color.Transparent;
+            this.UpKey2.Image = ((System.Drawing.Image)(resources.GetObject("UpKey2.Image")));
+            this.UpKey2.Location = new System.Drawing.Point(1046, 332);
+            this.UpKey2.Name = "UpKey2";
+            this.UpKey2.Size = new System.Drawing.Size(40, 40);
+            this.UpKey2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UpKey2.TabIndex = 50;
+            this.UpKey2.TabStop = false;
+            // 
+            // UpKey3
+            // 
+            this.UpKey3.BackColor = System.Drawing.Color.Transparent;
+            this.UpKey3.Image = ((System.Drawing.Image)(resources.GetObject("UpKey3.Image")));
+            this.UpKey3.Location = new System.Drawing.Point(1101, 221);
+            this.UpKey3.Name = "UpKey3";
+            this.UpKey3.Size = new System.Drawing.Size(40, 40);
+            this.UpKey3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UpKey3.TabIndex = 51;
+            this.UpKey3.TabStop = false;
+            // 
+            // Star5
+            // 
+            this.Star5.BackColor = System.Drawing.Color.Transparent;
+            this.Star5.Image = ((System.Drawing.Image)(resources.GetObject("Star5.Image")));
+            this.Star5.Location = new System.Drawing.Point(349, 207);
+            this.Star5.Name = "Star5";
+            this.Star5.Size = new System.Drawing.Size(40, 40);
+            this.Star5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star5.TabIndex = 52;
+            this.Star5.TabStop = false;
+            // 
+            // Star4
+            // 
+            this.Star4.BackColor = System.Drawing.Color.Transparent;
+            this.Star4.Image = ((System.Drawing.Image)(resources.GetObject("Star4.Image")));
+            this.Star4.Location = new System.Drawing.Point(349, 253);
+            this.Star4.Name = "Star4";
+            this.Star4.Size = new System.Drawing.Size(40, 40);
+            this.Star4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star4.TabIndex = 53;
+            this.Star4.TabStop = false;
+            // 
+            // Star3
+            // 
+            this.Star3.BackColor = System.Drawing.Color.Transparent;
+            this.Star3.Image = ((System.Drawing.Image)(resources.GetObject("Star3.Image")));
+            this.Star3.Location = new System.Drawing.Point(349, 299);
+            this.Star3.Name = "Star3";
+            this.Star3.Size = new System.Drawing.Size(40, 40);
+            this.Star3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star3.TabIndex = 54;
+            this.Star3.TabStop = false;
+            // 
+            // Star2
+            // 
+            this.Star2.BackColor = System.Drawing.Color.Transparent;
+            this.Star2.Image = ((System.Drawing.Image)(resources.GetObject("Star2.Image")));
+            this.Star2.Location = new System.Drawing.Point(349, 345);
+            this.Star2.Name = "Star2";
+            this.Star2.Size = new System.Drawing.Size(40, 40);
+            this.Star2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star2.TabIndex = 55;
+            this.Star2.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 511);
+            this.Controls.Add(this.Star2);
+            this.Controls.Add(this.Star3);
+            this.Controls.Add(this.Star4);
+            this.Controls.Add(this.Star5);
+            this.Controls.Add(this.UpKey3);
+            this.Controls.Add(this.UpKey2);
+            this.Controls.Add(this.Wall1);
+            this.Controls.Add(this.Wall2);
+            this.Controls.Add(this.Star1);
+            this.Controls.Add(this.Wall4);
+            this.Controls.Add(this.SideKey);
+            this.Controls.Add(this.Hurdle);
+            this.Controls.Add(this.One1);
+            this.Controls.Add(this.Wall5);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.UpKey1);
+            this.Controls.Add(this.Wall3);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.Wall3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpKey1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hurdle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SideKey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpKey2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpKey3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Wall3;
+        private System.Windows.Forms.PictureBox UpKey1;
+        private System.Windows.Forms.PictureBox Wall5;
+        private System.Windows.Forms.PictureBox One1;
+        private System.Windows.Forms.PictureBox Hurdle;
+        private System.Windows.Forms.PictureBox SideKey;
+        private System.Windows.Forms.PictureBox Wall4;
+        private System.Windows.Forms.PictureBox Star1;
+        private System.Windows.Forms.PictureBox Wall2;
+        private System.Windows.Forms.PictureBox Wall1;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox UpKey2;
+        private System.Windows.Forms.PictureBox UpKey3;
+        private System.Windows.Forms.PictureBox Star5;
+        private System.Windows.Forms.PictureBox Star4;
+        private System.Windows.Forms.PictureBox Star3;
+        private System.Windows.Forms.PictureBox Star2;
+    }
+}
