f251725d80be678ef9ce19b9fad3339c3bf187a5
park-jun <dnjsrktlsla@naver.com>
2023-11-28 13:17:06 +0900

    맵 추가

---
+++ WindowsFormsApp2/WindowsFormsApp1/Form4.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Wall1 = new System.Windows.Forms.PictureBox();
+            this.Wall3 = new System.Windows.Forms.PictureBox();
+            this.Wall2 = new System.Windows.Forms.PictureBox();
+            this.One1 = new System.Windows.Forms.PictureBox();
+            this.SideKey1 = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Star1 = new System.Windows.Forms.PictureBox();
+            this.Hurdle = new System.Windows.Forms.PictureBox();
+            this.flash = new System.Windows.Forms.PictureBox();
+            this.MovingBlock1 = new System.Windows.Forms.PictureBox();
+            this.One2 = new System.Windows.Forms.PictureBox();
+            this.One3 = new System.Windows.Forms.PictureBox();
+            this.Wall4 = new System.Windows.Forms.PictureBox();
+            this.SideKey2 = new System.Windows.Forms.PictureBox();
+            this.Wall5 = new System.Windows.Forms.PictureBox();
+            this.Star2 = new System.Windows.Forms.PictureBox();
+            this.One4 = new System.Windows.Forms.PictureBox();
+            this.Wall6 = new System.Windows.Forms.PictureBox();
+            this.Wall7 = new System.Windows.Forms.PictureBox();
+            this.Star3 = new System.Windows.Forms.PictureBox();
+            this.MovingBlock2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SideKey1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hurdle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flash)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MovingBlock1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SideKey2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MovingBlock2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Wall1
+            // 
+            this.Wall1.BackColor = System.Drawing.Color.Gray;
+            this.Wall1.Location = new System.Drawing.Point(1, 35);
+            this.Wall1.Name = "Wall1";
+            this.Wall1.Size = new System.Drawing.Size(40, 475);
+            this.Wall1.TabIndex = 0;
+            this.Wall1.TabStop = false;
+            // 
+            // Wall3
+            // 
+            this.Wall3.BackColor = System.Drawing.Color.Gray;
+            this.Wall3.Location = new System.Drawing.Point(1143, 35);
+            this.Wall3.Name = "Wall3";
+            this.Wall3.Size = new System.Drawing.Size(40, 475);
+            this.Wall3.TabIndex = 1;
+            this.Wall3.TabStop = false;
+            // 
+            // Wall2
+            // 
+            this.Wall2.BackColor = System.Drawing.Color.Gray;
+            this.Wall2.Location = new System.Drawing.Point(38, 97);
+            this.Wall2.Name = "Wall2";
+            this.Wall2.Size = new System.Drawing.Size(100, 40);
+            this.Wall2.TabIndex = 2;
+            this.Wall2.TabStop = false;
+            // 
+            // One1
+            // 
+            this.One1.BackColor = System.Drawing.Color.White;
+            this.One1.Location = new System.Drawing.Point(282, 97);
+            this.One1.Name = "One1";
+            this.One1.Size = new System.Drawing.Size(40, 40);
+            this.One1.TabIndex = 42;
+            this.One1.TabStop = false;
+            // 
+            // SideKey1
+            // 
+            this.SideKey1.BackColor = System.Drawing.Color.Coral;
+            this.SideKey1.Location = new System.Drawing.Point(1097, 189);
+            this.SideKey1.Name = "SideKey1";
+            this.SideKey1.Size = new System.Drawing.Size(40, 40);
+            this.SideKey1.TabIndex = 46;
+            this.SideKey1.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Blue;
+            this.Ball.Location = new System.Drawing.Point(38, 70);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(30, 30);
+            this.Ball.TabIndex = 47;
+            this.Ball.TabStop = false;
+            // 
+            // Star1
+            // 
+            this.Star1.BackColor = System.Drawing.Color.Yellow;
+            this.Star1.Location = new System.Drawing.Point(651, 189);
+            this.Star1.Name = "Star1";
+            this.Star1.Size = new System.Drawing.Size(40, 40);
+            this.Star1.TabIndex = 48;
+            this.Star1.TabStop = false;        
+            // 
+            // Hurdle
+            // 
+            this.Hurdle.BackColor = System.Drawing.Color.Black;
+            this.Hurdle.Location = new System.Drawing.Point(190, 143);
+            this.Hurdle.Name = "Hurdle";
+            this.Hurdle.Size = new System.Drawing.Size(901, 40);
+            this.Hurdle.TabIndex = 50;
+            this.Hurdle.TabStop = false;
+            // 
+            // flash
+            // 
+            this.flash.BackColor = System.Drawing.Color.SkyBlue;
+            this.flash.Location = new System.Drawing.Point(98, 60);
+            this.flash.Name = "flash";
+            this.flash.Size = new System.Drawing.Size(40, 40);
+            this.flash.TabIndex = 51;
+            this.flash.TabStop = false;
+            // 
+            // MovingBlock1
+            // 
+            this.MovingBlock1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.MovingBlock1.Location = new System.Drawing.Point(561, 97);
+            this.MovingBlock1.Name = "MovingBlock1";
+            this.MovingBlock1.Size = new System.Drawing.Size(530, 40);
+            this.MovingBlock1.TabIndex = 52;
+            this.MovingBlock1.TabStop = false;
+            // 
+            // One2
+            // 
+            this.One2.BackColor = System.Drawing.Color.White;
+            this.One2.Location = new System.Drawing.Point(374, 97);
+            this.One2.Name = "One2";
+            this.One2.Size = new System.Drawing.Size(40, 40);
+            this.One2.TabIndex = 53;
+            this.One2.TabStop = false;
+            // 
+            // One3
+            // 
+            this.One3.BackColor = System.Drawing.Color.White;
+            this.One3.Location = new System.Drawing.Point(469, 97);
+            this.One3.Name = "One3";
+            this.One3.Size = new System.Drawing.Size(40, 40);
+            this.One3.TabIndex = 54;
+            this.One3.TabStop = false;
+            // 
+            // Wall4
+            // 
+            this.Wall4.BackColor = System.Drawing.Color.Gray;
+            this.Wall4.Location = new System.Drawing.Point(605, 189);
+            this.Wall4.Name = "Wall4";
+            this.Wall4.Size = new System.Drawing.Size(40, 40);
+            this.Wall4.TabIndex = 55;
+            this.Wall4.TabStop = false;
+            // 
+            // SideKey2
+            // 
+            this.SideKey2.BackColor = System.Drawing.Color.Coral;
+            this.SideKey2.Location = new System.Drawing.Point(697, 327);
+            this.SideKey2.Name = "SideKey2";
+            this.SideKey2.Size = new System.Drawing.Size(40, 40);
+            this.SideKey2.TabIndex = 56;
+            this.SideKey2.TabStop = false;
+            // 
+            // Wall5
+            // 
+            this.Wall5.BackColor = System.Drawing.Color.Gray;
+            this.Wall5.Location = new System.Drawing.Point(469, 327);
+            this.Wall5.Name = "Wall5";
+            this.Wall5.Size = new System.Drawing.Size(40, 40);
+            this.Wall5.TabIndex = 57;
+            this.Wall5.TabStop = false;
+            // 
+            // Star2
+            // 
+            this.Star2.BackColor = System.Drawing.Color.Yellow;
+            this.Star2.Location = new System.Drawing.Point(515, 327);
+            this.Star2.Name = "Star2";
+            this.Star2.Size = new System.Drawing.Size(40, 40);
+            this.Star2.TabIndex = 58;
+            this.Star2.TabStop = false;
+            // 
+            // One4
+            // 
+            this.One4.BackColor = System.Drawing.Color.White;
+            this.One4.Location = new System.Drawing.Point(1097, 143);
+            this.One4.Name = "One4";
+            this.One4.Size = new System.Drawing.Size(40, 40);
+            this.One4.TabIndex = 61;
+            this.One4.TabStop = false;
+            // 
+            // Wall6
+            // 
+            this.Wall6.BackColor = System.Drawing.Color.Gray;
+            this.Wall6.Location = new System.Drawing.Point(561, 459);
+            this.Wall6.Name = "Wall6";
+            this.Wall6.Size = new System.Drawing.Size(40, 40);
+            this.Wall6.TabIndex = 62;
+            this.Wall6.TabStop = false;
+            // 
+            // Wall7
+            // 
+            this.Wall7.BackColor = System.Drawing.Color.Gray;
+            this.Wall7.Location = new System.Drawing.Point(47, 459);
+            this.Wall7.Name = "Wall7";
+            this.Wall7.Size = new System.Drawing.Size(40, 40);
+            this.Wall7.TabIndex = 63;
+            this.Wall7.TabStop = false;
+            // 
+            // Star3
+            // 
+            this.Star3.BackColor = System.Drawing.Color.Yellow;
+            this.Star3.Location = new System.Drawing.Point(47, 413);
+            this.Star3.Name = "Star3";
+            this.Star3.Size = new System.Drawing.Size(40, 40);
+            this.Star3.TabIndex = 64;
+            this.Star3.TabStop = false;
+            // 
+            // MovingBlock2
+            // 
+            this.MovingBlock2.BackColor = System.Drawing.Color.SaddleBrown;
+            this.MovingBlock2.Location = new System.Drawing.Point(139, 459);
+            this.MovingBlock2.Name = "MovingBlock2";
+            this.MovingBlock2.Size = new System.Drawing.Size(370, 40);
+            this.MovingBlock2.TabIndex = 65;
+            this.MovingBlock2.TabStop = false;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 511);
+            this.Controls.Add(this.MovingBlock2);
+            this.Controls.Add(this.Star3);
+            this.Controls.Add(this.Wall7);
+            this.Controls.Add(this.Wall6);
+            this.Controls.Add(this.One4);
+            this.Controls.Add(this.Star2);
+            this.Controls.Add(this.Wall5);
+            this.Controls.Add(this.SideKey2);
+            this.Controls.Add(this.Wall4);
+            this.Controls.Add(this.One3);
+            this.Controls.Add(this.One2);
+            this.Controls.Add(this.MovingBlock1);
+            this.Controls.Add(this.flash);
+            this.Controls.Add(this.Hurdle);
+            this.Controls.Add(this.Star1);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.SideKey1);
+            this.Controls.Add(this.One1);
+            this.Controls.Add(this.Wall2);
+            this.Controls.Add(this.Wall3);
+            this.Controls.Add(this.Wall1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Wall1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SideKey1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hurdle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flash)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MovingBlock1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SideKey2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.One4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MovingBlock2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Wall1;
+        private System.Windows.Forms.PictureBox Wall3;
+        private System.Windows.Forms.PictureBox Wall2;
+        private System.Windows.Forms.PictureBox One1;
+        private System.Windows.Forms.PictureBox SideKey1;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox Star1;
+        private System.Windows.Forms.PictureBox Hurdle;
+        private System.Windows.Forms.PictureBox flash;
+        private System.Windows.Forms.PictureBox MovingBlock1;
+        private System.Windows.Forms.PictureBox One2;
+        private System.Windows.Forms.PictureBox One3;
+        private System.Windows.Forms.PictureBox Wall4;
+        private System.Windows.Forms.PictureBox SideKey2;
+        private System.Windows.Forms.PictureBox Wall5;
+        private System.Windows.Forms.PictureBox Star2;
+        private System.Windows.Forms.PictureBox One4;
+        private System.Windows.Forms.PictureBox Wall6;
+        private System.Windows.Forms.PictureBox Wall7;
+        private System.Windows.Forms.PictureBox Star3;
+        private System.Windows.Forms.PictureBox MovingBlock2;
+    }
+}
+
